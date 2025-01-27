6f9f453ab0b304f68e8e664754b13a4981ac1c3d
Epicnic1 <nickvyvey@gmail.com>
2025-01-27 17:32:45 +0100

    end version Asteroids boot.dev

--- main.py
+++ main.py
@@ -2,18 +2,59 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+    asteroid_field = AsteroidField()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        for obj in updatable:
+            obj.update(dt) 
+        
         screen.fill((0,0,0))
+        
+        for obj in asteroids:
+            if obj.check_collision(player):
+                sys.exit("Game Over")
+
+            for shot in shots:
+                if shot.check_collision(obj):
+                    shot.kill()
+                    obj.split() 
+
+
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
